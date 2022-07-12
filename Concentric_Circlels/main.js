34f19290335dfb84be5c07323f074ba66d50be41
Vansh Verma <vanshverma@Vanshs-MacBook-Pro.local>
2022-07-12 08:54:40 +0530

    concentric circles

---
+++ Concentric_Circlels/main.js
@@ -0,0 +1,12 @@
+alert("Click on cirle's color to know which circle you clicking")
+function func1(){
+    alert("C1");
+    }
+ 
+function func2(){
+    alert("C2");
+    }
+ 
+function func3(){
+    alert("C3")
+    }
