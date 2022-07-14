67775ce6feb84c236ce7345f64dcdf9b0ac85eca
Vansh Verma <vanshverma@Vanshs-MacBook-Pro.local>
2022-07-14 22:17:47 +0530

    CurrencyConvertor done

--- Currency_Convertor/src/app/app.module.ts
+++ Currency_Convertor/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { ConvertorComponent } from './convertor/convertor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
